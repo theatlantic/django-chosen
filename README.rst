8f4983d446d924eb071ac5bf2eec529bdb43d6a8
Frankie Dintino <fdintino@theatlantic.com>
2011-07-29 16:06:41 -0400

    Initial commit

---
+++ README.rst
@@ -0,0 +1,8 @@
+=============
+django-chosen
+=============
+
+django-chosen is a project that makes available django FormFields that
+uses the `Chosen jQuery plugin`_.
+
+.. _Chosen jQuery plugin: http://harvesthq.github.com/chosen/
---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+try:
+	from setuptools import setup, find_packages
+except ImportError:
+	from ez_setup import use_setuptools
+	use_setuptools()
+	from setuptools import setup, find_packages
+
+setup(
+    name='django-chosen',
+    version="0.1",
+    description='django FormFields using the Chosen javascript plugin for jQuery',
+    author='Frankie Dintino',
+    author_email='fdintino@theatlantic.com',
+    url='https://github.com/theatlantic/django-chosen',
+    packages=find_packages(),
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+	include_package_data=True,
+	zip_safe=False,
+)
+
