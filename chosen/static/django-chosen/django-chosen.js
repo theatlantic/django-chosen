290349c22f03befa90881a4b8f60751cfce8a66e
Dylan Verheul <dylan.verheul@gmail.com>
2012-03-01 13:27:56 +0100

    Bug fix in django-chosen.js

--- chosen/static/django-chosen/django-chosen.js
+++ chosen/static/django-chosen/django-chosen.js
@@ -1,8 +1,12 @@
 if (typeof(jQuery) === "undefined") {
-    if (typeof("django") === "object" && typeof(django.jQuery) === "object") {
-        jQuery = django.jQuery;
+    if (typeof(django) !== "undefined") {
+        if (typeof(django.jQuery) !== "undefined") {
+            jQuery = django.jQuery;
+        } else {
+            throw "Cannot find jQuery in django";
+        }
     } else {
-        throw "Cannot find jQuery";
+        throw "Cannot find jQuery or django";
     }
 }
 jQuery(function() {
