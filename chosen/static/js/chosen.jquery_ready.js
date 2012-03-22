778de8661f1f0b4278d47866e0317e0f04a4e2ae
Frankie Dintino <fdintino@theatlantic.com>
2012-03-21 22:27:00 -0400

    Fix overflow:hidden issue with dropdown in django admin. fixes #12

--- chosen/static/js/chosen.jquery_ready.js
+++ chosen/static/js/chosen.jquery_ready.js
@@ -1,5 +1,24 @@
 (function ($) {
     $(function () {
-	    $(".chzn-select").chosen();
-	});
+        $(".chzn-select").each(function(i, select) {
+            var $select = $(select);
+
+            // Set overflow:visible on parent .form-row for django admin
+            $select.parents('.form-row').css('overflow', 'visible');
+
+            if (typeof grappelli == 'object') {
+                // Set overflow:visible on grappelli fieldset.module .row
+                $select.parents('.row').filter(function(i) {
+                    return $(this).parent('fieldset.module').length;
+                }).css('overflow', 'visible');
+                // Set overflow:visible on grappelli tabular module
+                $select.parents('td').filter(function(i) {
+                    return $(this).parent('.module.table').length;
+                }).css('overflow', 'visible');
+            }
+
+            // Initialize Chosen
+            $select.chosen();
+        });
+    });
 })((typeof window.django != 'undefined') ? django.jQuery : jQuery);
